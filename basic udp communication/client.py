ffd738b6431e4e288d0d1443311038bfeffd4b9a
LSD <piotrek.m@mail.com>
2019-04-14 12:40:35 +0200

    commit 2

--- basic udp communication/client.py
+++ basic udp communication/client.py
@@ -1,19 +1,26 @@
 from socket import *
+from datetime import datetime
 
 server_addres = ('127.0.0.1', 65535)
 message = []
-t = 3
-sckt = socket(AF_INET, SOCK_DGRAM)
+nickname = 'LSD'
+def log(message):
+     now = datetime.now().strftime('%H:%M:%S ')
+     return now+ message
 try:
+    sckt = socket(AF_INET, SOCK_DGRAM)
+    sckt.connect(server_addres)
     message_lenght = 0
     x = 0
     for x in range(t):
         gm = input(':')
         message.insert(x, gm)
+        #print(log('%s: %s'% (nickname, message[x])))
         message_lenght += len(message[x])
-        sent = sckt.sendto(bytes(message[x], 'utf-8'), server_addres)
+        sent = sckt.sendto(bytes(log(nickname+ ': '+ message[x]), 'utf-8'), server_addres)
         data, server = sckt.recvfrom(4096)
-    print('Send \033[1;35;m%s\033[1;37;m data \033[1;35;m%s \033[1;37;mtimes to \033[1;35;m%s \033[1;37;m' % (message_lenght, x+1, server_addres))
+        print(data.decode())
+    print(log('Send \033[1;35;m%s\033[1;37;m data to \033[1;35;m%s \033[1;37;m' % (message_lenght, server_addres)))
 except error:
         print("\033[1;31;mCould not open socket error:\033[1;37;m %s"% (error))
         sckt.close()
--- basic udp communication/server.py
+++ basic udp communication/server.py
@@ -1,18 +1,16 @@
 from socket import *
 
 server_address = ('localhost', 65535)
-sckt = socket(AF_INET, SOCK_DGRAM)
 print('starting up on \033[1;35;m%s \033[1;37;mport \033[1;35;m%s \033[1;37;m' % server_address)
-sckt.bind(server_address)
-
 timeout = 20
 try:
+    sckt = socket(AF_INET, SOCK_DGRAM)
+    sckt.bind(server_address)
     while True:
         print('Waiting to receive message\n')
         data, addr = sckt.recvfrom(4096)
-
         print('receive \033[1;35;m%s \033[1;37;mdata from \033[1;35;m%s \033[1;37;m' % (len(data), addr))
-        print(data)
+        '#print(data)'
 
         if data:
             sent = sckt.sendto(data, addr)
